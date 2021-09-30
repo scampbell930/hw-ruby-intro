f02445801921c9906484ea267505372ad0fd60d1
Sean Campbell <seancampbell0110@gmail.com>
2021-09-30 15:59:44 +0000

    Working on sum_to_n

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -12,11 +12,35 @@ def max_2_sum arr
   elsif arr.length() == 1
     return arr[0]
   else
-    
+    return arr.max(2).reduce(:+)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  map = {}
+
+  if arr.length() == 0 or arr.length() == 1
+    return false
+  else
+    # Search map for pair value for every element in array
+    for i in 0..(arr.length()-1)
+
+      # Check if element pair is in hash
+      if map.has_key?(n-arr[i])
+        return true
+
+      # Add element to hash
+      else
+        map.store(:arr[i], true)
+
+      end
+
+    end
+
+    # Didn't find pair value
+    return false
+
+  end
 end
 
 # Part 2
